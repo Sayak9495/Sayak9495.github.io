10dcc6186ac0b8c436c320893028a3fcda818df1
Sayak Sen <sayak1997sen@gmail.com>
2018-03-09 02:15:20 +0530

    uploaded code

--- games/sentiment analysis using afinn/sketch.js
+++ games/sentiment analysis using afinn/sketch.js
@@ -7,9 +7,30 @@ function preload(){
 function setup() {
 	noCanvas();
 	console.log(afinn);
+
+	var txt = select('#txt_area');
+	txt.input(typing);
+
+	function typing(){
+		var txtinput = txt.value();
+		var words = txt.split(/\W/);
+		var scoredwords=[];
+		var totalscore = 0;
+		for (var i=0;i<words.length; i++){
+			var word = words[i].toLowerCase();
+			if(afinn.hasOwnProperty(word)){
+				var score = Number(afinn[word]);
+				totalscore+=score;
+				scoredwords.push(word+': '+score);
+			}
+		}
+		var scorep=select('#score');
+		scorep.html('score: '+totalscore);
+		var wordlist = select('#wordlist');
+		wordlist.html(scoredwords)
+	}
 }
 
 function draw() {
 
 }
-
